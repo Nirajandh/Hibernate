6019f7aa98e60245c83f9723a7e142112d04b3cc
Nirajandh <151948730+Nirajandh@users.noreply.github.com>
2023-11-29 16:11:58 +0530

    Update CreateTableMain.java

--- featch_Data/CreateTableMain.java
+++ featch_Data/CreateTableMain.java
@@ -6,16 +6,15 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.internal.build.AllowSysOut;
 
+
+// in xml  file replace create with update or it will delete the existing data and then continue remaining part
+
 public class CreateTableMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        // for connection
 		SessionFactory factory=new Configuration().configure().buildSessionFactory();
-		
-	
-		
-
 		
 		Session session = factory.openSession();
 	
